85ef045c648fdb9e6bf623714aef9f0f7a3db3a9
siyaram-1137 <rdanuse0726@gmail.com>
2026-09-10 10:05:05 +0530

    Update HomeController.java

--- Spring_MVC_FS30_01/src/main/java/com/rt/cntrl/HomeController.java
+++ Spring_MVC_FS30_01/src/main/java/com/rt/cntrl/HomeController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-//	@RequestMapping("/")
-//	public String indexPage() {
-//		return "login";
-//	}
-	
 	@RequestMapping("/")
 	public String indexPage() {
 		return "login";
